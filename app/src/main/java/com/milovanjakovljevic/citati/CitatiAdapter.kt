ce5d30bf4f27e2d8c1dc52e4faf8483c1e61f0d4
Milovan99 <milovanjakovljevic78@gmail.com>
2022-12-10 15:20:50 +0100

    Initial commit

---
+++ app/src/main/java/com/milovanjakovljevic/citati/CitatiAdapter.kt
@@ -0,0 +1,31 @@
+package com.milovanjakovljevic.citati
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class CitatiAdapter : RecyclerView.Adapter<CitatiAdapter.CitatiViewHolder>(){
+
+    class CitatiViewHolder(v:View):RecyclerView.ViewHolder(v){
+        var view:View=v
+        var citat :String=""
+
+        fun bindCitat(citat:String){
+            this.citat=citat
+            view.textView.text=citat
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CitatiViewHolder {
+        return CitatiViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.recyclerview_item_row,parent,false))
+    }
+
+    override fun onBindViewHolder(holder: CitatiViewHolder, position: Int) {
+        holder.bindCitat("Sample quote")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
